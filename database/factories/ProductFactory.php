71e76c6266ae1435973b3b017102011bfff8a106
Ziel <frans_421_12@hotmail.com>
2021-05-14 18:25:20 -0500

    feat: ProductFactory name - word

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -25,10 +25,10 @@ class ProductFactory extends Factory
     {
         return [
             'code' => $this->faker->unique()->randomNumber(),
-            'name' => $this->faker->unique()->name(),
+            'name' => $this->faker->unique()->word(),
             'stock' => $this->faker->randomDigit,
 
-            'sell_price' => $this->faker->randomFloat(12,2),
+            'sell_price' => $this->faker->randomFloat(8,5),
             'status' => $this->faker->randomElement(['ACTIVE', 'DEACTIVATED']),
             'category_id' => Category::all()->random()->id,
             'provider_id' => Provider::all()->random()->id

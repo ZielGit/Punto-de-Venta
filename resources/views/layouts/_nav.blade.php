bff5cca0cd274fbb9efd19af84bc10768ee2e9ce
Ziel <frans_421_12@hotmail.com>
2021-05-25 20:31:10 -0500

    feat: Se arreglo los espacios admin y _nav

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,108 +1,108 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-      <ul class="nav">
-          <li class="nav-item nav-profile">
-            <div class="nav-link">
-              <div class="profile-image">
-                @if (Auth::user()->profile_photo_path)
-                  <img src="/storage/{{Auth::user()->profile_photo_path }}" alt="{{ Auth::user()->name }}" />
-                @else
-                  <img src="{{Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> 
-                @endif
-              </div>
-              <div class="profile-name">
-                <p class="name">
-                  {{ Auth::user()->name }}
-                </p>
-                <p class="designation">
-                  {{ Auth::user()->email }}
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('home')}}">
-              <i class="fa fa-home menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#page-layouts1" aria-expanded="false"
-                aria-controls="page-layouts">
-                <i class="fas fa-chart-line menu-icon"></i>
-                <span class="menu-title">Reportes</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="page-layouts1">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item d-none d-lg-block">
-                        <a class="nav-link" href="{{route('reports.day')}}">Reportes por día</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('reports.date')}}">Reportes por fecha</a>
-                    </li>
-                </ul>
-            </div>
-          </li>
-          @can('purchases.index')
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('purchases.index')}}">
-                <i class="fas fa-cart-plus menu-icon"></i>
-                <span class="menu-title">Compras</span>
-              </a>
-            </li>
-          @endcan
-          @can('sales.index')
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('sales.index')}}">
-                <i class="fas fa-shopping-cart menu-icon"></i>
-                <span class="menu-title">Ventas</span>
-              </a>
-            </li>
-          @endcan
-          @can('categories.index',)
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('categories.index')}}">
-                <i class="fas fa-tags menu-icon"></i>
-                <span class="menu-title">Categorias</span>
-              </a>
-            </li>
-          @endcan
-          @can('products.index')
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('products.index')}}">
-                  <i class="fas fa-boxes menu-icon"></i>
-                  <span class="menu-title">Productos</span>
-              </a>
-            </li>
-          @endcan
-          @can('clients.index')
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('clients.index')}}">
-                  <i class="fas fa-users menu-icon"></i>
-                  <span class="menu-title">Clientes</span>
-              </a>
-            </li>
-          @endcan
-          @can('providers.index')
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('providers.index')}}">
-                <i class="fas fa-shipping-fast menu-icon"></i>
-                <span class="menu-title">Proveedores</span>
-              </a>
-            </li>
-          @endcan
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('users.index')}}">
-                <i class="fas fa-user-tag menu-icon"></i>
-                <span class="menu-title">Usuarios</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('roles.index')}}">
-                <i class="fas fa-user-cog menu-icon"></i>
-                <span class="menu-title">Roles</span>
-            </a>
-          </li>
-      </ul>
+  <ul class="nav">
+      <li class="nav-item nav-profile">
+        <div class="nav-link">
+          <div class="profile-image">
+            @if (Auth::user()->profile_photo_path)
+              <img src="/storage/{{Auth::user()->profile_photo_path }}" alt="{{ Auth::user()->name }}" />
+            @else
+              <img src="{{Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> 
+            @endif
+          </div>
+          <div class="profile-name">
+            <p class="name">
+              {{ Auth::user()->name }}
+            </p>
+            <p class="designation">
+              {{ Auth::user()->email }}
+            </p>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('home')}}">
+          <i class="fa fa-home menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#page-layouts1" aria-expanded="false"
+            aria-controls="page-layouts">
+            <i class="fas fa-chart-line menu-icon"></i>
+            <span class="menu-title">Reportes</span>
+            <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="page-layouts1">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="{{route('reports.day')}}">Reportes por día</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('reports.date')}}">Reportes por fecha</a>
+                </li>
+            </ul>
+        </div>
+      </li>
+      @can('purchases.index')
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('purchases.index')}}">
+            <i class="fas fa-cart-plus menu-icon"></i>
+            <span class="menu-title">Compras</span>
+          </a>
+        </li>
+      @endcan
+      @can('sales.index')
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('sales.index')}}">
+            <i class="fas fa-shopping-cart menu-icon"></i>
+            <span class="menu-title">Ventas</span>
+          </a>
+        </li>
+      @endcan
+      @can('categories.index',)
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('categories.index')}}">
+            <i class="fas fa-tags menu-icon"></i>
+            <span class="menu-title">Categorias</span>
+          </a>
+        </li>
+      @endcan
+      @can('products.index')
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('products.index')}}">
+              <i class="fas fa-boxes menu-icon"></i>
+              <span class="menu-title">Productos</span>
+          </a>
+        </li>
+      @endcan
+      @can('clients.index')
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('clients.index')}}">
+              <i class="fas fa-users menu-icon"></i>
+              <span class="menu-title">Clientes</span>
+          </a>
+        </li>
+      @endcan
+      @can('providers.index')
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('providers.index')}}">
+            <i class="fas fa-shipping-fast menu-icon"></i>
+            <span class="menu-title">Proveedores</span>
+          </a>
+        </li>
+      @endcan
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('users.index')}}">
+            <i class="fas fa-user-tag menu-icon"></i>
+            <span class="menu-title">Usuarios</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('roles.index')}}">
+            <i class="fas fa-user-cog menu-icon"></i>
+            <span class="menu-title">Roles</span>
+        </a>
+      </li>
+  </ul>
 </nav>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,116 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>@yield('title')</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="{{asset('melody/vendors/iconfonts/font-awesome/css/all.min.css')}}">
-  <link rel="stylesheet" href="{{asset('melody/vendors/css/vendor.bundle.base.css')}}">
-  <link rel="stylesheet" href="{{asset('melody/vendors/css/vendor.bundle.addons.css')}}">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('melody/css/style.css')}}">
-  @yield('styles')
-  <!-- endinject -->
-  <link rel="shortcut icon" href="http://www.urbanui.com/" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('melody/vendors/iconfonts/font-awesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('melody/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('melody/vendors/css/vendor.bundle.addons.css')}}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{asset('melody/css/style.css')}}">
+    @yield('styles')
+    <!-- endinject -->
+    <link rel="shortcut icon" href="http://www.urbanui.com/" />
 </head>
 <body class="sidebar-dark">
-  <!-- Agrege el tema sidebar-dark en el body-->
-  <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
-    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="#"><img src="{{asset('melody/images/logo.svg')}}" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="#"><img src="{{asset('melody/images/logo-mini.svg')}}" alt="logo"/></a>
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-stretch">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-          <span class="fas fa-bars"></span>
-        </button>
-        <ul class="navbar-nav navbar-nav-right">
-          @yield('create')
-          <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-              @if (Auth::user()->profile_photo_path)
-                <img src="/storage/{{Auth::user()->profile_photo_path }}" alt="{{ Auth::user()->name }}" />
-              @else
-                <img src="{{Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> 
-              @endif
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-              <a class="dropdown-item" href="{{route('profile.show')}}">
-                <i class="fas fa-cog text-primary"></i>
-                Configuración
-              </a>
-              <div class="dropdown-divider"></div>
-              <form action="{{route('logout')}}" method="post">
-                @csrf
-                <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();this.closest('form').submit();">
-                  <i class="fas fa-power-off text-primary"></i>
-                  Cerrar Sesión
-                </a>
-              </form>
-              {{-- form --}}
+    <!-- Agrege el tema sidebar-dark en el body-->
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo" href="#"><img src="{{asset('melody/images/logo.svg')}}" alt="logo"/></a>
+                <a class="navbar-brand brand-logo-mini" href="#"><img src="{{asset('melody/images/logo-mini.svg')}}" alt="logo"/></a>
             </div>
-          </li>
-          
-          @yield('options')
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="fas fa-bars"></span>
-        </button>
-      </div>
-    </nav>
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      
-      @yield('preference')
-      <!-- partial -->
-      <!-- partial:partials/_sidebar.html -->
-      @include('layouts._nav')
-      
-      <!-- partial -->
-      <div class="main-panel">
-        @yield('content')
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018. All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
-          </div>
-        </footer>
+            <div class="navbar-menu-wrapper d-flex align-items-stretch">
+                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                    <span class="fas fa-bars"></span>
+                </button>
+                <ul class="navbar-nav navbar-nav-right">
+                    @yield('create')
+                    <li class="nav-item nav-profile dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+                        @if (Auth::user()->profile_photo_path)
+                            <img src="/storage/{{Auth::user()->profile_photo_path }}" alt="{{ Auth::user()->name }}" />
+                        @else
+                            <img src="{{Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> 
+                        @endif
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                            <a class="dropdown-item" href="{{route('profile.show')}}">
+                                <i class="fas fa-cog text-primary"></i>
+                                Configuración
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <form action="{{route('logout')}}" method="post">
+                                @csrf
+                                <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();this.closest('form').submit();">
+                                <i class="fas fa-power-off text-primary"></i>
+                                Cerrar Sesión
+                                </a>
+                            </form>
+                            {{-- form --}}
+                        </div>
+                    </li>
+                    @yield('options')
+                </ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                <span class="fas fa-bars"></span>
+                </button>
+            </div>
+        </nav>
         <!-- partial -->
-      </div>
-      <!-- main-panel ends -->
+        <div class="container-fluid page-body-wrapper">
+        
+            @yield('preference')
+            <!-- partial -->
+            <!-- partial:partials/_sidebar.html -->
+            @include('layouts._nav')
+            
+            <!-- partial -->
+            <div class="main-panel">
+                @yield('content')
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018. All rights reserved.</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
+                    </div>
+                </footer>
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
+    <!-- container-scroller -->
 
-  <!-- plugins:js -->
-  <script src="{{asset('melody/vendors/js/vendor.bundle.base.js')}}"></script>
-  <script src="{{asset('melody/vendors/js/vendor.bundle.addons.js')}}"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="{{asset('melody/js/off-canvas.js')}}"></script>
-  <script src="{{asset('melody/js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('melody/js/misc.js')}}"></script>
-  <script src="{{asset('melody/js/settings.js')}}"></script>
-  <script src="{{asset('melody/js/todolist.js')}}"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="{{asset('melody/js/dashboard.js')}}"></script>
-  <!-- End custom js for this page-->
-  @yield('scripts')
+    <!-- plugins:js -->
+    <script src="{{asset('melody/vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{asset('melody/vendors/js/vendor.bundle.addons.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{asset('melody/js/off-canvas.js')}}"></script>
+    <script src="{{asset('melody/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('melody/js/misc.js')}}"></script>
+    <script src="{{asset('melody/js/settings.js')}}"></script>
+    <script src="{{asset('melody/js/todolist.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{asset('melody/js/dashboard.js')}}"></script>
+    <!-- End custom js for this page-->
+    @yield('scripts')
 
 </body>
-
 
 </html>

4244d4d69bb85fe0eba511d7630f5f44cd6f73d7
Ziel <frans_421_12@hotmail.com>
2021-05-31 01:01:10 -0500

    feat: Vista show de categorias

---
+++ resources/views/admin/category/show.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.admin')
+@section('title','Productos de categoría')
+@section('styles')
+@endsection
+@section('content')
+<div class="content-wrapper">
+    <div class="page-header">
+        <h3 class="page-title">
+            Productos que pertenecen a {{$category->name}}
+        </h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('home')}}">Panel administrador</a></li>
+                <li class="breadcrumb-item"><a href="{{route('categories.index')}}">Categorías</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{$category->name}}</li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="row">
+        <div class="col mb-2">
+            <a href="{{route('products.create')}}">
+                <span class="btn btn-primary">Agregar</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table id="order-listing" class="table">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Nombre</th>
+                                    <th>Stock</th>
+                                    <th>Estado</th>
+                                    <th>Categoría</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($category->products as $product)
+                                <tr>
+                                    <th scope="row">{{$product->id}}</th>
+                                    <td>
+                                        <a href="{{route('products.show',$product)}}">{{$product->name}}</a>
+                                    </td>
+                                    <td>{{$product->stock}}</td>
+
+                                    @if ($product->status == 'ACTIVE')
+                                    <td>
+                                        <a class="jsgrid-button btn btn-success" href="{{route('change.status.products', $product)}}" title="Editar">
+                                            Activo <i class="fas fa-check"></i>
+                                        </a>
+                                    </td>
+                                    @else
+                                    <td>
+                                        <a class="jsgrid-button btn btn-danger" href="{{route('change.status.products', $product)}}" title="Editar">
+                                            Desactivado <i class="fas fa-times"></i>
+                                        </a>
+                                    </td>
+                                    @endif
+                                    
+                                    <td>{{$product->category->name}}</td>
+                                    <td>
+                                        {{-- Error al eliminar, no redirecciona a products.index ni con laravel collective --}}
+                                        <form action="{{route('products.destroy', $product)}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <a href="{{route('products.edit', $product)}}" title="Editar">
+                                                <span class="btn btn-outline-info"><i class="fas fa-edit"></i></span>
+                                            </a>
+                                            
+                                            <button class="btn btn-outline-danger" type="submit" title="Eliminar">
+                                                <i class="far fa-trash-alt"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+                <div class="card-footer text-muted">
+                    <a href="{{route('categories.index')}}" class="btn btn-primary float-right">Regresar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+@section('scripts')
+{!! Html::script('melody/js/profile-demo.js') !!}
+<script src="{{asset('melody/js/data-table.js')}}"></script>
+@endsection

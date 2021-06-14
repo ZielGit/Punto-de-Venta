45cfff55659a23c7a75d0804907e32bccc86dea9
Ziel <frans_421_12@hotmail.com>
2021-06-14 00:36:34 -0500

    feat: Se agrego form label y text

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -28,12 +28,13 @@
                     <form action="{{route('users.store')}}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="name">Nombre</label>
-                            <input type="text" name="name" id="name" class="form-control" placeholder="" aria-describedby="helpId">
+                            {!! Form::label('Nombre') !!}
+                            {!! Form::text('name','', ['class' => 'form-control']) !!}
                             @error('name')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
+
                         <div class="form-group">
                             <label for="email">Correo electrónico</label>
                             <input type="email" name="email" id="email" class="form-control" placeholder="" aria-describedby="helpId">

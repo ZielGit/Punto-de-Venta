f77d940719cbfe32a7a2a2316b60852ad6cc50bf
Ziel <frans_421_12@hotmail.com>
2022-02-28 15:16:49 -0500

    Cambio de DB raw a selectRaw

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,8 @@ class HomeController extends Controller
         // $ventasmes=DB::select('SELECT monthname(v.sale_date) as mes, sum(v.total) as totalmes from sales v where v.status="VALID" group by monthname(v.sale_date) order by month(v.sale_date) desc limit 12');
         
         // $ventasdia=DB::select('SELECT DATE_FORMAT(v.sale_date,"%d/%m/%Y") as dia, sum(v.total) as totaldia from sales v where v.status="VALID" group by v.sale_date order by day(v.sale_date) desc limit 15');
-        $ventasdia = Sale::where('status', 'VALID')->select( DB::raw("DATE_FORMAT(sale_date, '%d/%m/%Y') as date") )
+        $ventasdia = Sale::where('status', 'VALID')
+            ->selectRaw("DATE_FORMAT(sale_date, '%d/%m/%Y') as date")
             ->selectRaw('sum(total) as total')->groupBy('date')->take(30)->get();
 
         $mostSelledProducts = Product::join('sale_details', 'products.id', '=', 'sale_details.product_id')
